5769a6e551b6d74beec47949b87e34df4e4e2ee7
csakou <c.sakoumpentas@protonmail.com>
2020-04-29 23:43:43 +0300

    Added documentation and simplified the code

--- player.py
+++ player.py
@@ -1,20 +1,51 @@
-#!/home/csakou/Documents/Python/hang_man/venv/bin/python3
+"""
+A class used to represent a Player
+
+Attributes
+----------
+
+name : str
+    A string between 1-16 charactres representing the players name.
+
+Methods
+-------
+
+validate_name(name):
+    Checks if the name given by the user is valid.
+
+get_name():
+    Returns the players name
+"""
 
 class Player:
     def __init__(self):
-        self.set_name()
+        """
+        Initializes Player object and requests the name of the player
+        """
+        print("Hello player, please input your name. It must be between 1-16 characters:")
+        name = input('> ')
+        self.name = self.validate_name(name)
 
-    def set_name(self):
-        print('Hello player. Please enter your name. It must have 1-16 characters:')
-        while True:
-            self.name = input("> ")
-            if len(self.name) <= 0 or len(self.name) > 16:
-                raise ValueError('Your name must be in between 1-16 characters:')
-                continue
-            break
+    def validate_name(self, name):
+        """
+        Validates if the name of the player is in the correct form
+
+        Parameters
+        ----------
+
+        name : str
+            The name the user inputted
+        """
+        if len(name) <= 0 or len(name) > 16:
+            raise ValueError('Your name must be in between 1-16 characters:')
+        return name
 
     def get_name(self):
-        return self.name
+        """
+        Returns
+        --------
 
-if __name__ == "__main__":
-    player = Player()
+        name : str
+            The name of the player
+        """
+        return self.name
